7c97702add7d70d1db98aee1812e1c32ff23de76
Arrielle Kooiman <arrielle.kooiman@gmail.com>
2017-03-01 11:04:45 -0600

    after lecture

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -13,11 +13,12 @@ $(document).ready(function(){
           var currentBook = response[i]; //More legible for code below
           var $newBook = $('<tr>'); //create a new row for each book
           $newBook.data('id', currentBook.id); //adds data ID to the new book object so we can call it later
-          $newBook.append('<td>' + currentBook.title + '</td>'); //show user the title
-          $newBook.append('<td>' + currentBook.author + '</td>'); //show user the author
-          $newBook.append('<td>' + currentBook.edition + '</td>'); //show user the edition
-          $newBook.append('<td>' + currentBook.publisher + '</td>');//show user the publisher
-          $newBook.append('<td><button class="deleteButton">Delete</button>'); //create a delete button
+          $newBook.append('<td><input value="' + currentBook.title + '" class="bookTitle"></td>'); //show user the title
+          $newBook.append('<td><input value="' + currentBook.author + '" class="bookAuthor"></td>'); //show user the author
+          $newBook.append('<td><input value="' + currentBook.edition + '" class="bookEdition"></td>'); //show user the edition
+          $newBook.append('<td><input value="' + currentBook.publisher + '" class="bookPublisher"></td>');//show user the publisher
+          $newBook.append('<td><button class="deleteButton">Delete</button>');
+          $newBook.append('<td><button class="saveButton">Save</button>') //create a delete button
           $('#bookShelf').prepend($newBook);
         }
       }
@@ -45,6 +46,30 @@ $(document).ready(function(){
       }
     });
   });
+
+  $('#bookShelf').on('click', '.saveButton', function(){
+    var bookIDSave = $(this).parent().parent().data().id;
+    //where are we?
+    var titleOfBookToSave = $(this).parent().parent().find('.bookTitle').val();
+    var authorOfBookToSave = $(this).parent().parent().find('.bookAuthor').val();
+    var editionOfBookToSave = $(this).parent().parent().find('.bookEdition').val();
+    var publisherOfBookToSave = $(this).parent().parent().find('.bookEdition').val();
+    //create a book objects
+    var bookObjectToSave = {
+      title: titleOfBookToSave
+    }
+    // var bookNewSaveObject =
+    console.log('SaveButton has ID: ', bookIDSave);
+  $.ajax({
+    type: 'PUT', //it's the PG update PUT or PATCH
+    url: '/books/save/' + bookIDSave,
+    data: bookObjectToSave,// books/delete/48 (where 48 is bookIDDelete)
+    success: function(response){
+      console.log(response);
+      getBookData();
+    }//end success
+  });//end ajax
+  });//end on click
 
 
 $('#bookShelf').on('click', '.deleteButton', function(){
--- server/routes/books.js
+++ server/routes/books.js
@@ -47,16 +47,16 @@ router.post('/new', function(req, res){
       // We connected to the database!!!
       // Now, we're gonna' git stuff!!!!!
       client.query('INSERT INTO books (title, author, edition, publisher) VALUES ($1, $2, $3, $4);',
-        [newBook.title, newBook.author, newBook.edition, newBook.publisher],
-        function(errorMakingQuery, result){
-          done();
-          if(errorMakingQuery) {
-            console.log('Error making the database query: ', errorMakingQuery);
-            res.sendStatus(500);
-          } else {
-            res.sendStatus(201);
-          }
-        });
+      [newBook.title, newBook.author, newBook.edition, newBook.publisher],
+      function(errorMakingQuery, result){
+        done();
+        if(errorMakingQuery) {
+          console.log('Error making the database query: ', errorMakingQuery);
+          res.sendStatus(500);
+        } else {
+          res.sendStatus(201);
+        }
+      });
     }
   });
 });
@@ -76,18 +76,48 @@ router.delete('/delete/:id', function(req, res){
       // We connected to the database!!!
       // Now, we're gonna' git stuff!!!!!
       client.query('DELETE FROM books WHERE id=$1;', //PARAM 1 $1 tells PG that we're looking for a variable
-        [bookID], //PARAM 2 variable that we're adding to the PG query
-        function(errorMakingQuery, result){ //PARAM 3
-          done();
-          if(errorMakingQuery) {
-            console.log('Error making the database query: ', errorMakingQuery);
-            res.sendStatus(500);
-          } else {
-            res.sendStatus(201);
-          }//ends client.query function
-        });//ends client.query
-      } //ends ppol connect function
-    });//ends pool connect
+      [bookID], //PARAM 2 variable that we're adding to the PG query (Replaces $1 in the query)
+      function(errorMakingQuery, result){ //PARAM 3 the function that is run after the query takes place
+        done();
+        if(errorMakingQuery) {
+          console.log('Error making the database query: ', errorMakingQuery);
+          res.sendStatus(500);
+        } else {
+          res.sendStatus(201);
+        }//ends client.query function
+      });//ends client.query
+    } //ends ppol connect function
+  });//ends pool connect
 });//ends delete router
+
+
+//delete/48  /:id tells us that it was an optional perameter
+router.put('/save/:id', function(req, res){
+  var bookID = req.params.id; //finds the optional parameter
+  var bookObject = req.body;
+  console.log('book id to save: ', bookID);
+
+  pool.connect(function(errorConnectingToDatabase, client, done){
+    if(errorConnectingToDatabase) {
+      // There was an error connecting to the database
+      console.log('Error connecting to database: ', errorConnectingToDatabase);
+      res.sendStatus(500);
+    } else {
+      // We connected to the database!!!
+      // Now, we're gonna' git stuff!!!!!
+      client.query('UPDATE books SET title=$1 WHERE id=$2;', //PARAM 1 $1 tells PG that we're looking for a variable
+      [bookObject.title, bookID], //PARAM 2 variable that we're adding to the PG query (Replaces $1 in the query)
+      function(errorMakingQuery, result){ //PARAM 3 the function that is run after the query takes place
+        done();
+        if(errorMakingQuery) {
+          console.log('Error making the database query: ', errorMakingQuery);
+          res.sendStatus(500);
+        } else {
+          res.sendStatus(200);
+        }//ends client.query function
+      });//ends client.query
+    } //ends ppol connect function
+  });//ends pool connect
+});//ends save router
 
 module.exports = router;
